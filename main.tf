6adf0b0e32ffa00579493a8be803598ac6234c45
vijay reddy <vijayreddy0555@gmail.com>
2024-03-22 08:30:06 +0000

    change the code

--- main.tf
+++ main.tf
@@ -1,18 +1,19 @@
 provider "azurerm" {
-  features {}
+  features {
+    
+  }
 }
 
-# Create a resource group
-resource "azurerm_resource_group" "rg" {
-  name     = "my-resource-group"
-  location = "eastus"
+resource "azurerm_resource_group" "example" {
+  name                      = local.resource_group_name
+  location                  = var.location
 }
 
-# Create a storage account
-resource "azurerm_storage_account" "st" {
-  name                     = "mystorageaccount"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+resource "azurerm_storage_account" "example" {
+  name                      = local.storage_account_name
+  resource_group_name       = azurerm_resource_group.example.name
+  location                  = var.location
+  account_tier              = "Standard"
+  account_replication_type  = "LRS"
 }
+
